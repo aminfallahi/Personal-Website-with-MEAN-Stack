5346e0c6062a7a6fea085384f2362dc89c1c3f32
Klee6 <kylelee6@gmail.com>
2019-03-31 20:55:50 -0400

    Comments and fixes, you may need to test on your PC to see if it works
    I had to reinstall babel in order to use the import syntax you used. It may not work on your PC if you use linux.
    
    I think we're probably going to demo on your computer because I dont have mongoDb installed locally, you could try to use the connection string to the online database instead but for demo probably on computer is better.

--- personal-website/backend/server.js
+++ personal-website/backend/server.js
@@ -11,6 +11,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 mongoose.connect('mongodb://127.0.0.1:27017/pw');
+//mongoose.connect('mongodb+srv://CSE775:775@cluster0-u4bmz.mongodb.net/test?retryWrites=true');
 
 const connection = mongoose.connection;
 
@@ -18,8 +19,28 @@ connection.once('open', () => {
     console.log('MongoDB database connection established successfully!');
 });
 
-app.use('/', router);
+app.use('/', router); //Applies the routes to our application
 
+/* If you want to run middleware that will run on every request */
+
+/*router.use(function(req, res, next) {
+
+    // log each request to the console
+    console.log(req.method, req.url);
+
+    // continue doing what we were doing and go to the route
+    next(); 
+});*/
+
+
+/*Routing with parameters*/
+/*router.get('/test/:name', function(req, res) {
+    res.send('test ' + req.params.name + '!');
+});*/
+
+
+//Later on We should partition this into its seperate folder called routes to cleanup this code
+//Get
 router.route('/msgs').get((req, res) => {
     Msg.find((err, msgs) => {
         if (err)
@@ -29,6 +50,7 @@ router.route('/msgs').get((req, res) => {
     });
 });
 
+//Post
 router.route('/contact').post((req, res) => {
     let msg = new Msg(req.body);
     msg.save()
